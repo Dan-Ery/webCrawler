366498810af1ddf00c4c75435cf24ce0a2424ead
Dan-Ery <daniel.eryavec@gmail.com>
2024-03-06 02:50:35 -0500

    feat: Add crawler :)

--- crawl.js
+++ crawl.js
@@ -30,6 +30,49 @@ function getURLsFromHTML(htmlBody, baseURL) {
     return absoluteURLs
 }
 
+async function fetchPage(pageURLString) {
+    url = new URL(pageURLString)
+    let response = await fetch(url, {
+        method: "GET",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        }
+    });
+    if (response.status >= 400) {
+        console.log(`Page request for ${pageURL} failed: Status ${response.status}`)
+        return
+    }
+    contentType = response.headers.get('content-type')
+    if (!contentType.startsWith("text/html")) {
+        console.log(`Page request for ${pageURLString} returned invalid content type: ${contentType}`)
+        return
+    }
+    return await response.text()
+}
+
+async function crawlPage(baseURL, currentURL= baseURL, pages = {}) {
+    if (new URL(baseURL).hostname != new URL(currentURL).hostname) {
+        return pages
+    }
+    let normalizedURL = normalizeURL(currentURL)
+    if (pages[normalizedURL]) {
+        pages[normalizedURL]++
+        return pages
+    } else {
+        pages[normalizedURL] = 1
+
+        console.log(`Searching urls on ${normalizedURL} . . .`)
+        pageHTML = await fetchPage(currentURL)
+        linksInPage = getURLsFromHTML(pageHTML, baseURL)
+        for (let link of linksInPage) {
+            await crawlPage(baseURL, link, pages)
+        }
+
+        return pages
+    }
+}
+
 module.exports = {
-    normalizeURL, getURLsFromHTML
+    normalizeURL, getURLsFromHTML, crawlPage
 }
--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 const { argv } = require('node:process');
-const { normalizeURL, getURLsFromHTML } = require('./crawl.js')
+const { normalizeURL, getURLsFromHTML, crawlPage } = require('./crawl.js')
 
-function main(){
+async function main(){
     if (argv.length !== 3) {
         throw new Error("")
     }
     baseUrl = argv[2]
     console.log(baseUrl)
+    console.log(await crawlPage(baseUrl))
   }
   
   main()
